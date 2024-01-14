47b43217cf993ef26a654b5e47f4907a02d433ec
GLinBoy <hojjatabedie@gmail.com>
2024-01-14 13:10:43 +0100

    📦 NEW: Added editorconfig plugin to Gradle
    resolved #11

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     kotlin("jvm") version "1.9.21"
     kotlin("plugin.spring") version "1.9.21"
     kotlin("plugin.jpa") version "1.9.21"
+    id ("org.ec4j.editorconfig") version "0.0.3"
 }
 
 group = "com.glinboy.test.springboot"
