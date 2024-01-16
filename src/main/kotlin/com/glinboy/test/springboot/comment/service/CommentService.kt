44cc641e2583331caf4d46072e9ca58d6dc10600
GLinBoy <hojjatabedie@gmail.com>
2024-01-16 20:44:13 +0100

    📦 NEW: Added CommentService
    #17

---
+++ src/main/kotlin/com/glinboy/test/springboot/comment/service/CommentService.kt
@@ -0,0 +1,12 @@
+package com.glinboy.test.springboot.comment.service
+
+import com.glinboy.test.springboot.comment.service.dto.CommentDTO
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+
+interface CommentService {
+    fun getComments(pageable: Pageable): Page<CommentDTO>
+    fun getComment(id: Long): CommentDTO?
+    fun saveComment(commentDTO: CommentDTO): CommentDTO
+    fun deleteComment(id: Long)
+}
