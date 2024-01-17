49e0ec404ee943840954693eae9841a733c74a2c
GLinBoy <hojjatabedie@gmail.com>
2024-01-17 18:56:06 +0100

    👌 IMPROVE: Added OpenAPI to the project
    resolved #22

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.modelmapper:modelmapper:3.2.0")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("org.postgresql:postgresql")
