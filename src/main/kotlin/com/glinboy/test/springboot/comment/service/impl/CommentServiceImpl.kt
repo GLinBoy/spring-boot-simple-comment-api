485abc4d2815f6199d28cf2da9c421c67a878006
GLinBoy <hojjatabedie@gmail.com>
2024-01-17 19:02:53 +0100

    👌 IMPROVE: Updated GetComments method in comment service impl.

--- src/main/kotlin/com/glinboy/test/springboot/comment/service/impl/CommentServiceImpl.kt
+++ src/main/kotlin/com/glinboy/test/springboot/comment/service/impl/CommentServiceImpl.kt
@@ -1,26 +1,27 @@
 package com.glinboy.test.springboot.comment.service.impl
 
+import com.glinboy.test.springboot.comment.entity.Comment
+import com.glinboy.test.springboot.comment.repository.CommentRepository
 import com.glinboy.test.springboot.comment.service.CommentService
 import com.glinboy.test.springboot.comment.service.dto.CommentDTO
+import org.modelmapper.ModelMapper
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 
 @Service
-class CommentServiceImpl: CommentService {
-    override fun getComments(pageable: Pageable): Page<CommentDTO> {
-        TODO("Not yet implemented")
-    }
+class CommentServiceImpl(
+    val commentRepository: CommentRepository,
+    val modelMapper: ModelMapper
+) : CommentService {
+    override fun getComments(pageable: Pageable): Page<CommentDTO> =
+        commentRepository.findAll(pageable).map {
+            modelMapper.map<CommentDTO>(it, Comment::class.java)
+        }
 
-    override fun getComment(id: Long): CommentDTO? {
-        TODO("Not yet implemented")
-    }
+    override fun getComment(id: Long): CommentDTO? = throw NotImplementedError()
 
-    override fun saveComment(commentDTO: CommentDTO): CommentDTO {
-        TODO("Not yet implemented")
-    }
+    override fun saveComment(commentDTO: CommentDTO): CommentDTO = throw NotImplementedError()
 
-    override fun deleteComment(id: Long) {
-        TODO("Not yet implemented")
-    }
+    override fun deleteComment(id: Long) = throw NotImplementedError()
 }
