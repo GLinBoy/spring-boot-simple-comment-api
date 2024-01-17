31258172923072582d897f209418ed59e2e4be84
GLinBoy <hojjatabedie@gmail.com>
2024-01-17 19:04:24 +0100

    👌 IMPROVE: Added API to returns all comments

--- src/main/kotlin/com/glinboy/test/springboot/comment/web/api/CommentResource.kt
+++ src/main/kotlin/com/glinboy/test/springboot/comment/web/api/CommentResource.kt
@@ -1,6 +1,12 @@
 package com.glinboy.test.springboot.comment.web.api
 
 import com.glinboy.test.springboot.comment.service.CommentService
+import com.glinboy.test.springboot.comment.service.dto.CommentDTO
+import io.swagger.v3.oas.annotations.Parameter
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -8,5 +14,9 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/v1/comments")
 class CommentResource(val commentService: CommentService) {
+
+    @GetMapping
+    fun getComments(@Parameter(hidden = true) pageable: Pageable): ResponseEntity<Page<CommentDTO>> =
+        ResponseEntity.ok(commentService.getComments(pageable))
 
 }
