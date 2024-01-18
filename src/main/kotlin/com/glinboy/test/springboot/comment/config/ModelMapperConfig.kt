f412ff32dfd70bfff645e08c50fe8dc0bf0f01a4
GLinBoy <hojjatabedie@gmail.com>
2024-01-18 19:43:38 +0100

    👌 IMPROVE: Added converters to the ModelMapper config.

--- src/main/kotlin/com/glinboy/test/springboot/comment/config/ModelMapperConfig.kt
+++ src/main/kotlin/com/glinboy/test/springboot/comment/config/ModelMapperConfig.kt
@@ -1,13 +1,20 @@
 package com.glinboy.test.springboot.comment.config
 
+import org.modelmapper.AbstractConverter
 import org.modelmapper.ModelMapper
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.util.UUID
 
 @Configuration
 class ModelMapperConfig {
     @Bean
-    fun modelMapper(): ModelMapper = ModelMapper()
+    fun modelMapper(): ModelMapper {
+        val modelMapper = ModelMapper()
+        modelMapper.addConverter(UUIDToStringConverter())
+        modelMapper.addConverter(StringToUUIDConverter())
+        return modelMapper
+    }
 }
 
 class UUIDToStringConverter: AbstractConverter<UUID, String>() {
