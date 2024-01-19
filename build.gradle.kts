abe83c66ca237a4f56e1f41aa3777d0993c73c59
GLinBoy <hojjatabedie@gmail.com>
2024-01-19 20:34:19 +0100

    👌 IMPROVE: Extracted OpenAPI version
    #28

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,8 @@ repositories {
     mavenCentral()
 }
 
+val springDocVersion = "2.3.0"
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
@@ -28,7 +30,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.modelmapper:modelmapper:3.2.0")
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:${springDocVersion}")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("org.postgresql:postgresql")
