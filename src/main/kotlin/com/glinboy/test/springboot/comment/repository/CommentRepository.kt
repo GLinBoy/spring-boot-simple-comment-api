3cb4393d4b73e4a2447cf22390f060113cee47bb
GLinBoy <hojjatabedie@gmail.com>
2024-01-21 13:20:14 +0100

    👌 IMPROVE: Added a method that return comments of a page to CommentRepository
    #32

--- src/main/kotlin/com/glinboy/test/springboot/comment/repository/CommentRepository.kt
+++ src/main/kotlin/com/glinboy/test/springboot/comment/repository/CommentRepository.kt
@@ -1,8 +1,11 @@
 package com.glinboy.test.springboot.comment.repository
 
 import com.glinboy.test.springboot.comment.entity.Comment
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
 
 interface CommentRepository: JpaRepository<Comment, UUID> {
+    fun findAllByPageId(pageId: String, pageable: Pageable): Page<Comment>
 }
