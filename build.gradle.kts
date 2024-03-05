16ecd00aaa846dfd0b15b962ec8ce1ba87879167
GLinBoy <glinboy@live.com>
2024-03-05 20:11:45 +0100

    👌 IMPROVE: Upgraded dependency ben-manes.versions [0.50.0 -> 0.51.0]
    #46

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     kotlin("plugin.spring") version "1.9.22"
     kotlin("plugin.jpa") version "1.9.22"
     id ("org.ec4j.editorconfig") version "0.0.3"
-    id ("com.github.ben-manes.versions") version "0.50.0"
+    id ("com.github.ben-manes.versions") version "0.51.0"
 }
 
 group = "com.glinboy.test.springboot"
